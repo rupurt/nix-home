b5f81b83cb01bf1d40dcb24becefb67646dc076e
Alex Kwiatkowski <alex+git@fremantle.io>
2025-08-11 19:38:53 -0700

    enable copilot lua and cmp

--- home/neovim/plugins/copilot-chat.nix
+++ home/neovim/plugins/copilot-chat.nix
@@ -1,5 +1,5 @@
 {
   programs.nixvim.plugins.copilot-chat = {
-    enable = true;
+    enable = false;
   };
 }
--- home/neovim/plugins/copilot-lua.nix
+++ home/neovim/plugins/copilot-lua.nix
@@ -1,5 +1,13 @@
 {
   programs.nixvim.plugins.copilot-lua = {
-    enable = false;
+    enable = true;
+    settings = {
+      panel = {
+        enabled = false;
+      };
+      suggestion = {
+        enabled = false;
+      };
+    };
   };
 }
