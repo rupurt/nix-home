1956cc778b9a80e26cfc6318e5eb5437ea5f83a5
Alex Kwiatkowski <alex+git@fremantle.io>
2025-12-19 14:30:07 -0800

    add opencode to path

--- home/zsh.nix
+++ home/zsh.nix
@@ -43,7 +43,7 @@
 
     sessionVariables = {
       STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-      PATH="$HOME/.local/bin:$PATH:$HOME/.local/share/rialo/bin:$PATH";
+      PATH="$HOME/.local/bin:$HOME/.opencode/bin:$HOME/.local/share/rialo/bin:$PATH";
     };
 
     # initExtra = ''
